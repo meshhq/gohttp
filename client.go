b91c01f52ba1f83193d23705aa4ac086b788b1a0
Kevin Coleman <kevin@layer.com>
2015-11-17 22:24:01 -0800

    Updating structs

--- client.go
+++ client.go
@@ -5,13 +5,14 @@ import (
 	"net/http"
 )
 
+// Client models an HTTP client
 type Client struct {
 	BaseURL string
 	Headers http.Header
 	Client  *http.Client
 }
 
-// HTTPRequest models an HTTP request sumbitted to the controller
+// Request models an HTTP request sumbitted to the controller
 // for processing.
 type Request struct {
 
@@ -25,8 +26,36 @@ type Request struct {
 	Body map[string]interface{}
 }
 
+const (
+	// GET is a constant for the HTTP GET method.
+	GET = "GET"
+
+	// POST is a constant for the HTTP POST method.
+	POST = "POST"
+
+	// DELETE is a constant for the HTTP DELETE method.
+	DELETE = "DELETE"
+
+	// PATCH is a constant for the HTTP PATCH method.
+	PATCH = "PATCH"
+)
+
 func NewClient(baseURL string, headers http.Header) Client {
 	return Client{baseURL, headers, &http.Client{}}
+}
+
+func (c *Client) Execute(r *Request) (map[string]interface{}, error) {
+	switch r.Method {
+	case GET:
+		return c.Get(r.URL)
+	case POST:
+		return c.Post(r.URL, r.Body)
+	case DELETE:
+		return c.Delete(r.URL)
+	case PATCH:
+		return c.Patch(r.URL, r.Body)
+	}
+	return nil, nil
 }
 
 func (c *Client) Get(url string) (map[string]interface{}, error) {
@@ -83,14 +112,6 @@ func (c *Client) Patch(url string, params map[string]interface{}) (map[string]in
 	return performRequest(req, c.Client)
 }
 
-//------------------------------------------------------------------------------
-// @Block Based Requests
-//------------------------------------------------------------------------------
-
-func (c *Client) PostWithBlock(r Request, s func(http.Request, map[string]interface{}), f func(http.Request, error)) {
-
-}
-
 func performRequest(r *http.Request, c *http.Client) (map[string]interface{}, error) {
 	fmt.Printf("Performing Request: %v\n", r)
 	resp, err := c.Do(r)
@@ -99,8 +120,17 @@ func performRequest(r *http.Request, c *http.Client) (map[string]interface{}, er
 	}
 	defer resp.Body.Close()
 	fmt.Printf("Response: %v\n", resp)
-	if resp.StatusCode != http.StatusOK || resp.StatusCode != http.StatusCreated {
+	if (resp.StatusCode != http.StatusOK) && (resp.StatusCode != http.StatusCreated) {
+		err = fmt.Errorf("Unprocessable response code encountered: %v", resp.StatusCode)
 		return nil, err
 	}
 	return ParseJSON(resp.Body)
+}
+
+//------------------------------------------------------------------------------
+// @Block Based Requests
+//------------------------------------------------------------------------------
+
+func (c *Client) PostWithBlock(r Request, s func(http.Request, map[string]interface{}), f func(http.Request, error)) {
+
 }
